c516dcf1293852ccbcbfbe4bef3738b624267b7e
Sycondrin13 <97759218+Sycondrin13@users.noreply.github.com>
2022-02-02 13:35:59 -0600

    main

--- bot.py
+++ bot.py
@@ -1,2 +1,26 @@
+import os
 from twitchio.ext import commands
 
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        super().__init__(token='', prefix='?', initial_channels=['rainbownomja'])
+
+    async def event_ready(self):
+        # We are logged in and ready to chat and use commands...
+        print(f'Logged in as | {self.nick}')
+
+    @commands.command()
+
+    async  def potato(self, ctx: commands.Context):
+        await  ctx.send(f'You are a potato')
+
+    async def hello(self, ctx: commands.Context):
+        # Send a hello back!
+        await ctx.send(f'Hello {ctx.author.name}!')
+
+
+bot = Bot()
+bot.run()
